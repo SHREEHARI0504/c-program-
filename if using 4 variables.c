45fbf7467fe2a544da1c22a3832f3e48a6d3e770
SHREEHARI0504 <113368958+SHREEHARI0504@users.noreply.github.com>
2022-09-14 18:15:26 +0530

    Add files via upload

---
+++ if using 4 variables.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{ 
+     int num;
+    
+   
+    printf("Enter the three numbers\n");
+   
+    scanf("%d",&num);
+    
+     
+    if(num%2==0)
+    
+        printf("even");
+    else
+        printf("odd");
+    
+    
+     
+    
+   
+}
+
+
