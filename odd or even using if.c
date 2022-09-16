316bf2f54fe537807a70e61ee4104fc62e847bcd
SHREEHARI0504 <113368958+SHREEHARI0504@users.noreply.github.com>
2022-09-16 18:09:00 +0530

    Add files via upload

---
+++ odd or even using if.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{ 
+     int num;
+    
+   
+    printf("Enter the three numbers\n");
+   
+    scanf("%d",&num);
+    
+     
+    if(num%2==0)
+    
+        printf("even");
+    else
+        printf("odd");
+    
+    
+     
+    
+   
+}
+
