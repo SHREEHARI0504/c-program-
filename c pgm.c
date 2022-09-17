aed3b1eddc1489a31e886089a30b4164d4c6002a
SHREEHARI0504 <113368958+SHREEHARI0504@users.noreply.github.com>
2022-09-17 18:33:32 +0530

    Add files via upload

---
+++ c pgm.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{ 
+     int a,b,c;
+    
+   
+   // printf("Enter the three numbers\n");
+   
+    //scanf("%d %d %d",&a,&b,&c);
+    
+    a=10;
+    b=20;
+    c=a&&(b=30);
+    printf("%d %d %d",a,b,c);
+   
+    
+    return 0;
+    
+   
+}
+
