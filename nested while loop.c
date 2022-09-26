56e87fc0434d05d44491f2799d99cf610b3fe305
SHREEHARI P <113368958+SHREEHARI0504@users.noreply.github.com>
2022-09-26 21:52:42 +0530

    Add files via upload

---
+++ nested while loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{ 
+    int a,b;
+    b=1;
+    while(b<=5)
+    {
+       a=1;
+       while(a<=5)
+       {
+           printf("%d",a);
+           a=a+1;
+       }
+    printf("\n");   
+    b=b+1;
+    //
+    }
+}
