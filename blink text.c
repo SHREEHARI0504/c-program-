05541640bef1cc5830d8e65d5864857996aaf9c6
SHREEHARI P <113368958+SHREEHARI0504@users.noreply.github.com>
2022-10-12 19:07:27 +0530

    Add files via upload

---
+++ blink text.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<Windows.h>
+
+int main(){
+    int i;
+    for(i=0; i<10; i++)
+    {
+      printf(".....WEL COME.....");
+      Sleep(500);
+      system("cls");
+      Sleep(500);
+    }
+   
+return 0;
+}
